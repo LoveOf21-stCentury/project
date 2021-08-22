74a81bc28ca54b90511d9997a9c7029057f48c2a
Lyubov <l.musilmanbekova12@gmail.com>
2021-08-22 21:09:34 +0300

    first commit

---
+++ 1-st lesson.js
@@ -0,0 +1,57 @@
+/* const person = {
+    firstname: 'Lyubov',
+    lastname: 'Gulyaeva',
+    year: 2000,
+    languages: ['ru', 'en'],
+    hasHusband: false,
+    greet: function() {
+        console.log('greet to me');
+    }
+}
+
+console.log(person.languages);
+console.log(person['languages']);
+const key = 'firstname'
+console.log(person[key]);
+person.hasHusband = true;
+console.log(person);
+person.isProgrammer = true;
+persone.greet() */
+
+
+//Numbers
+/* const num = 45
+const float = 45.45
+const pow = 10e3 //возведение чисел
+
+console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER); // максмальное выражение которым мы можем пользоваться в операциях
+console.log(Number.MIN_SAFE_INTEGER);
+console.log('MAX_VALUE', Number.MAX_VALUE); //максимальное значение 
+console.log('MIN_VALUE', Number.MIN_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY); /* если что-то делим на ноль, то получаем Infinity */
+/* console.log(Number.NaN);
+console.log(typeof NaN); //каламбур
+const you = 2/undefined; //NaN
+console.log(Number.isNaN(54)) //проверка на тип. Число ли
+console.log(Number.isFinite(Infinity));
+console.log(Number.isFinite(42));
+
+const stringInt = '42'
+const stringFloat = '42.42'
+console.log(parseInt(stringInt) + 2);
+console.log(+(stringInt) + 2);
+
+console.log(parseFloat(stringFloat) + 2);
+console.log(+(stringFloat) + 2);  */
+
+
+/* console.log((0.4 + 0.2));
+console.log(+(0.4 + 0.2).toFixed(4))
+console.log(parseFloat((0.4 + 0.2).toFixed(4))); */ //превратилось в строку этот способ просто обрезает и делает из нее строку\
+
+//BigInt
+//console.log(10n - BigInt(4)); //при делении части после запятой отсекаются
+
+
+
---
+++ script.js
@@ -0,0 +1,45 @@
+/* let num_1 = 5;
+let num_2 = 3.9859;
+
+console.log('Plus is ' + (num_1 + num_2));
+
+let num_3 = 10;
+num_3--;
+console.log('result ' + num_3);
+
+let str_1 = parseInt('12');
+    str_2 = Math.round('15');
+
+    console.log('result ' + (str_1 + str_2));
+
+    console.log (Math.round(num_2));
+/*  
+console.log('Math ' + Math.PI);
+console.log('Math ' + Math.min(4, 6, 4, 3, 90));
+console.log('Math ' + Math.max(4, 6, 4, 3, 90)); */
+
+/* let num = 15;
+
+if (num == 25) {
+    console.log('done');
+} else {
+console.log('yep');
+} */
+
+'use srtict'
+/* 
+console.log(typeof(5 + '')); //string too
+
+const num = 6;
+
+console.log('https://' + num); */
+    
+//const fontSize = 26 + 'px';
+/* 
+console.log(typeof(+'4'));
+ */
+
+/* console.log(typeof(!!'434dds')); */
+
+
+
